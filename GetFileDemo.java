4d939d0dc9b3c08f1ddc107f649d4372aab30790
Akash Bhatthal <akashbhatthal@gmail.com>
2024-11-13 16:00:36 -0800

    Initial commit

---
+++ GetFileDemo.java
@@ -0,0 +1,48 @@
+import org.scec.getfile.GetFile;
+import org.scec.getfile.BackupManager;
+
+import java.lang.Thread;
+import java.io.File;
+import java.net.URI;
+
+public class GetFileDemo {
+	public static void main(String[] Args) {
+		GetFile gf = new GetFile(
+			/*clientMetaFile=*/new File("getfile.json"),
+			/*serverMetaURI=*/URI.create("http://localhost:8088/meta.json"));
+		BackupManager bm = gf.getBackupManager();
+		BackupManager bm1 = gf.getBackupManager("1"); // New instance
+		BackupManager bm2 = gf.getBackupManager(); // Same instance as bm
+		bm.backup();
+		bm1.backup();
+
+		gf.updateAll();
+		System.out.println("Updated");
+		myWait();
+
+		bm1.rollback();
+		System.out.println("Rolled back to before update.");
+		bm1.backup();
+		myWait();
+
+		gf.updateFile("file2");
+		System.out.println("Update just file2");
+		myWait();
+
+		bm2.rollback();
+		System.out.println("Roll back to after update.");
+		myWait();
+		
+		System.out.println("Rolled back to before update.");
+		bm1.rollback();
+	}
+
+	public static void myWait() {
+		try {
+			Thread.sleep(5000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# GetFile Demo
+This demo shows how GetFile can be used to update files and manage snapshots.
+Only GetFile and BackupManager are available for use outside the package.
+
+All instances of BackupManager can only be made in the context of a GetFile
+instance via GetFile.getBackupManager. Only the main GetFile class is required
+for file updating.
+
+## Terminal 1: Run server
+cd getfile-demo/server
+python3 -m http.server 8088
+
+## Terminal 2: Run GetFileDemo
+cd getfile-demo
+javac -cp lib/getfile-all.jar GetFileDemo.java
+java -cp .:lib/getfile-all.jar GetFileDemo
+
